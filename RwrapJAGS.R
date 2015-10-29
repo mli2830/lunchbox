5096af6f53865ace8a974ec6007161e5f1290b65
mikelin2 <wzmichael.li@gmail.com>
2015-10-29 17:00:36 -0400

    it works

--- RwrapJAGS.R
+++ RwrapJAGS.R
@@ -1,14 +1,14 @@
 library(R2jags)
 source('Isimulator.R')
 
-sirdat <- simi(beta=2,gamma = 0.4,pop = 100,i0 = 5,t0 = 1,
+sirdat <- simi(beta=2,gamma = 0.4,pop = 50,i0 = 5,t0 = 1,
                end = 50,dt = 1,report = 0.3, seed=101)
 
 sirdat
 
 
 
-jagsdat <- list(o=sirdat[,3], dt=1,M=nrow(sirdat), i0=10, pop=100)
+jagsdat <- list(o=sirdat[,3], dt=1,M=nrow(sirdat), i0=2, pop=50)
 parameters <- c("beta","gamma","report")
 inits <- list(list(beta=2,gamma=0.3,report=0.4))
 
