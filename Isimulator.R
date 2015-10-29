d706a40d782a5dad46c4138e35e67616ddde7737
mikelin2 <wzmichael.li@gmail.com>
2015-10-29 16:54:57 -0400

    It works but beta estimate from are off

--- Isimulator.R
+++ Isimulator.R
@@ -1,7 +1,7 @@
 simi <- function(beta = 0.5, gamma=0.5, pop=50, i0=1,
-                   t0=1, end=20, dt=1, report=0.5){
+                   t0=1, end=20, dt=1, report=0.5,seed=NULL){
     if (!is.null(seed)) set.seed(seed)
-    tvec <- squ(1,end,by=dt)
+    tvec <- seq(1,end,by=dt)
     n <- length(tvec)
     I <- numeric(n)
     Iobs <- numeric(n)
@@ -9,8 +9,8 @@ simi <- function(beta = 0.5, gamma=0.5, pop=50, i0=1,
     ##Initial conditions
     I[1] <- i0
     Pr <- 1 - exp(-gamma*dt)
-    Pi <- 1 - exp(-beta*I[1]*dt/N)
-    inf <- rbinom(1, prob=Pi, size= N-I[1])
+    Pi <- 1 - exp(-beta*I[1]*dt/pop)
+    inf <- rbinom(1, prob=Pi, size= pop-I[1])
     rec <- rbinom(1, prob=Pr, size = I[1])
     Iobs <- rbinom(1, prob=report, size = I[1])
 
@@ -18,10 +18,10 @@ simi <- function(beta = 0.5, gamma=0.5, pop=50, i0=1,
 
     for (t in 2:n){
         I[t] <- I[t-1] - rec + inf
-        Pi <- 1 - exp(-beta*I[t]*dt/N)
-        inf <- rbinom(1, prob=Pi, size = N-I[t])
+        Pi <- 1 - exp(-beta*I[t]*dt/pop)
+        inf <- rbinom(1, prob=Pi, size = pop-I[t])
         rec <- rbinom(1, prob=Pr, size = I[t])
-        Iobs[i] <- rbinom(1, prob=report, size = I[t])
+        Iobs[t] <- rbinom(1, prob=report, size = I[t])
     }
 
     cbind(tvec, I, Iobs)
---
+++ RwrapJAGS.R
@@ -0,0 +1,20 @@
+library(R2jags)
+source('Isimulator.R')
+
+sirdat <- simi(beta=2,gamma = 0.4,pop = 100,i0 = 5,t0 = 1,
+               end = 50,dt = 1,report = 0.3, seed=101)
+
+sirdat
+
+
+
+jagsdat <- list(o=sirdat[,3], dt=1,M=nrow(sirdat), i0=10, pop=100)
+parameters <- c("beta","gamma","report")
+inits <- list(list(beta=2,gamma=0.3,report=0.4))
+
+
+j1 <- jags(data=jagsdat,inits, param=parameters, model.file = "Imodel.bug",   
+           n.chains=length(inits), n.iter=3000, n.burnin=500, n.thin=37)
+
+
+j1
