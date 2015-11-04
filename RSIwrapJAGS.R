117522f3863ce8101cecbb3231021be445499483
bbolker <bbolker@gmail.com>
2015-11-04 13:15:00 -0500

    more comments

--- RSIwrapJAGS.R
+++ RSIwrapJAGS.R
@@ -1,39 +1,42 @@
 library(R2jags)
 library(mcmcplots)
+library(lattice)
+library(coda)
+
 source('SIsimulator.R')
 
 sirdat <- simSI(beta=0.05,i0=1,s0 = 100,t0 = 2,
-                Pobs=0.5,
-                end = 20, seed=101)
+                Pobs=0.5, end = 20, seed=101)
 
 sirdat
 
 jagsdat <- list(Iobs=sirdat$Iobs, M=nrow(sirdat),i0=1)
 parameters <- c("beta","Pobs","s0")
 
-## so how do we set a good initial value without knowing it??
+## Two chains from fairly divergent starting points
+## (s0 starts at real value)
+## set initial I to observed I +1 so that we never have a case
+## with Iobs=0, I>0
 inits <- list(list(beta=0.1,Pobs=0.2,
                    I=sirdat$Iobs+1,s0=100),
               list(beta=0.02,Pobs=0.7,I=sirdat$Iobs+1,s0=100))
 
 
-## http://sourceforge.net/p/mcmc-jags/bugs/22/
+## When using JAGS 3.2.0 we sometimes get:
+## "Unable to create conjugate sampler
+## Please send a bug report to martyn_plummer@users.sourceforge.net"
+## web-search reveals this workaround from
+##    http://sourceforge.net/p/mcmc-jags/bugs/22/:
 rjags::set.factory("bugs::Conjugate", FALSE, type="sampler")
 j1 <- jags(data=jagsdat,inits, param=parameters, 
            model.file= "SImodel.bug" ,
            n.chains=length(inits), n.iter=10000, n.burnin=0)
 
-## ???
-## Unable to create conjugate sampler
-## Please send a bug report to martyn_plummer@users.sourceforge.net
 
-library(lattice)
-library(coda)
-library(mcmcplots)
 m1 <- as.mcmc(j1)
-xyplot(m1)
-splom(as.matrix(m1),pch=".")
-mcmcplot(m1)
-plot(j1)
+xyplot(m1)                    ## traceplot
+splom(as.matrix(m1),pch=".")  ## pairs plot
+mcmcplot(m1)                  ## fancy plot
+plot(j1)                      ## another fancy (but less useful) plot
 
 
--- SIsimulator.R
+++ SIsimulator.R
@@ -1,13 +1,13 @@
 ##' Basic chain binomial simulator
-##' @param beta 
+##' @param beta prob. of adequate contact per infective
 ##' @param i0 initial infectives
 ##' @param s0 initial susceptibles
 ##' @param t0 initial time (unused)
 ##' @param end ending time
-##' @param seed
+##' @param seed random number seed
 ##' @param Pobs observation probability (1 by default)
 ##' @return a data frame with columns (time, S, I, R)
-##' @details This is a Reed-Frost model.
+##' @details This is a Reed-Frost model.  N=s0+i0.
 ##' @examples
 ##' set.seed(101)
 ##' s1 <- simSI(beta=0.02,Pobs=0.7)
@@ -17,9 +17,9 @@ simSI <- function(beta = 0.02, i0=1, s0=99,
                  t0=1, end=20, Pobs=1, seed=NULL){
 
   ## BMB: change name to "chain-binomial" ? e.g., simCB?
+  ## *all* infecteds recover in the next time step
     
   ## default params: R0=beta*N=2
-    
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,end)
   n <- length(tvec)
@@ -33,13 +33,11 @@ simSI <- function(beta = 0.02, i0=1, s0=99,
   ## e.g. see http://depts.washington.edu/sismid09/software/Module_7/reedfrost.R
   ## or the somewhat lame Wikipedia page
   
-  ## Generate the Unobserved process I, and observables
-
   ## should we scale force of infection by N or not??
   ## if we don't scale _explicitly_ then R0 = beta*N
 
-  ## chain binomial model: *all* infecteds recover in the
-  ## next time step
+  ## Generate the Unobserved process I, and observables:
+
   for (t in 2:n){
     I[t] <- rbinom(1,prob=Psi,size=S[t-1])
     S[t] <- S[t-1] - I[t]
@@ -51,7 +49,3 @@ simSI <- function(beta = 0.02, i0=1, s0=99,
   data.frame(time=tvec, S, I, R, Iobs)
 
 }
-
-
-
-
