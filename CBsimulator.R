17e13ea4c86b32a5bedca2703aca32e8e8831826
mikelin2 <wzmichael.li@gmail.com>
2015-11-26 10:37:02 -0500

    get someone to test if it works

--- CBsimulator.R
+++ CBsimulator.R
@@ -7,40 +7,34 @@
 ##' @param numobs ending time
 ##' @param seed random number seed
 ##' @param reporting observation probability (1 by default)
-##' @return a data frame with columns (time, S, I, R)
-##' @details This is a Reed-Frost model.  N=s0+i0 (not any-more CB? or Mike's CBS)
+##' @return a data frame with columns (time, S, I, R) 
 simCB <- function(beta = 0.02, N=100, effprop=0.9, i0=1,
                   t0=1, numobs=20, reporting=1, seed=NULL){
   
-  ## BMB: change name to "chain-binomial" ? e.g., simCB? (done)
   ## *all* infecteds recover in the next time step
   
-  ## default params: R0=beta*N=2 (does R0 still have the same meaning now that I switch things around? I think it is still the same.)
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,numobs)
   n <- length(tvec)
-  I <- Iobs <- S <- R <- numeric(n)
+  I <- Iobs <- S <- R <- pSI <- numeric(n)
   
   ##Initial conditions
   N0 <- round(effprop*N)
   I[1] <- i0
   S[1] <- N0 - i0
   R[1] <- N-N0
-  Psi <- 1 - (1-beta)^I[1]
+  pSI[1] <- 1 - (1-beta)^I[1]
   Iobs[1] <- rbinom(1,prob=reporting,size=I[1])## Reed-Frost
   ## e.g. see http://depts.washington.edu/sismid09/software/Module_7/reedfrost.R
   ## or the somewhat lame Wikipedia page
   
-  ## should we scale force of infection by N or not??
-  ## if we don't scale _explicitly_ then R0 = beta*N
-  
   ## Generate the Unobserved process I, and observables:
   
   for (t in 2:n){
-    I[t] <- rbinom(1,prob=Psi,size=S[t-1])
+    I[t] <- rbinom(1,prob=pSI[t-1],size=S[t-1])
     S[t] <- S[t-1] - I[t]
     R[t] <- R[t-1] + I[t-1]
-    Psi <- 1 - (1-beta)^I[t]
+    pSI <- 1 - (1-beta)^I[t]
     Iobs[t] <- rbinom(1,prob=reporting,size=I[t])
   }
   
