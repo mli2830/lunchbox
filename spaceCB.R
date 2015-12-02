028b8aba13aae1d578b8e532b38b99bdf6bab478
mikelin2 <wzmichael.li@gmail.com>
2015-12-01 22:36:26 -0500

    time to finish the spatial simulator

--- spaceCB.R
+++ spaceCB.R
@@ -16,29 +16,29 @@ simspaceCB <- function(spacenum = 1,betatemp = 0.02, N=100, effprop=0.9, i0=1,
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,numobs)
   n <- length(tvec)
-  S <- matrix(0,ncol=spacenum,nrow=numobs)
+  S <- I <- matrix(0,ncol=spacenum,nrow=numobs)
   R <- Iobs <- numeric(numobs)
-  pSI <- I <- array(0,dim=c(numobs,spacenum,spacenum))
+  pSI <- array(0,dim=c(numobs,spacenum,spacenum))
   
   
   
   ##Initial conditions
   N0 <- round(effprop*N)
-  I[1,1,1] <- i0 
+  I[1,1] <- i0 
   S[1,] <- round(N0/spacenum)
   S[1,1] <- S[1,1]-I[1,1,1]
   R[1] <- N-N0
   pSI[1,,] <- 0
   beta <- array(betatemp,dim=c(numobs,spacenum,spacenum))
-  pSI[1,1,1] <- 1 - (1-beta[1,1,1])^I[1,1,1]
-  Iobs[1] <- rbinom(1,prob=reporting,size=sum(I[1,,]))
+  pSI[1,1,1] <- 1 - (1-beta[1,1,1])^I[1,1]
+  Iobs[1] <- rbinom(1,prob=reporting,size=sum(I[1,]))
   
   ## Generate the Unobserved process I, and observables:
   
   for (t in 2:n){
     for (i in 1:spacenum){
       for (j in 1:spacenum){
-        I[t,i,j] <- rbinom(1,prob=pSI[t-1,i,j],size=S[t-1,i])
+        I[t,i] <- rbinom(1,prob=pSI[t-1,i,j],size=S[t-1,i])
         pSI[t,i,j] <- 1 - (1-beta[t,i,j])^sum(I[t,,j])
       }
       S[t,i] <- S[t-1,i] - sum(I[t,i,])
