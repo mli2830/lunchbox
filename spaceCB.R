3335713a2552f0e6d68288551828fe42cd135d7e
mikelin2 <wzmichael.li@gmail.com>
2015-12-02 02:02:54 -0500

    space simulator done

--- spaceCB.R
+++ spaceCB.R
@@ -8,7 +8,7 @@
 ##' @param seed random number seed
 ##' @param reporting observation probability (1 by default)
 ##' @return a data frame with columns (time, S, I, R) 
-simspaceCB <- function(spacenum = 1,betatemp = 0.02, N=100, effprop=0.9, i0=1,
+simspaceCB <- function(spacenum = 1,betaw = 0.02, betab = 0.01, N=100, effprop=0.9, i0=1,
                        t0=1, numobs=20, reporting=1, seed=NULL){
   
   ## *all* infecteds recover in the next time step
@@ -16,39 +16,53 @@ simspaceCB <- function(spacenum = 1,betatemp = 0.02, N=100, effprop=0.9, i0=1,
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,numobs)
   n <- length(tvec)
-  S <- I <- matrix(0,ncol=spacenum,nrow=numobs)
+  S  <- I <- pSI <- matrix(0,ncol=spacenum,nrow=numobs)
+  beta <- matrix(betab,nrow=spacenum, ncol=spacenum)
   R <- Iobs <- numeric(numobs)
-  pSI <- array(0,dim=c(numobs,spacenum,spacenum))
-  
+#  pSI <- I <-array(0,dim=c(numobs,spacenum,spacenum))
   
   
   ##Initial conditions
   N0 <- round(effprop*N)
   I[1,1] <- i0 
   S[1,] <- round(N0/spacenum)
-  S[1,1] <- S[1,1]-I[1,1,1]
+  S[1,1] <- S[1,1]-I[1,1]
   R[1] <- N-N0
-  pSI[1,,] <- 0
-  beta <- array(betatemp,dim=c(numobs,spacenum,spacenum))
-  pSI[1,1,1] <- 1 - (1-beta[1,1,1])^I[1,1]
+  pSI[1,] <- 0
+  for(i in 1:spacenum){
+    beta[i,i] <- betaw
+  }
+  for (i in 1:spacenum){
+    temp <- spacenum
+    for (j in 1:spacenum){
+      temp <- temp - (1-beta[i,j])^I[1,j] 
+    }
+    pSI[1,i] <- min(temp,1)
+  }
+  
   Iobs[1] <- rbinom(1,prob=reporting,size=sum(I[1,]))
   
   ## Generate the Unobserved process I, and observables:
   
   for (t in 2:n){
     for (i in 1:spacenum){
-      for (j in 1:spacenum){
-        I[t,i] <- rbinom(1,prob=pSI[t-1,i,j],size=S[t-1,i])
-        pSI[t,i,j] <- 1 - (1-beta[t,i,j])^sum(I[t,,j])
-      }
-      S[t,i] <- S[t-1,i] - sum(I[t,i,])
+        I[t,i] <- rbinom(1,prob=pSI[t-1,i],size=S[t-1,i])
+        S[t,i] <- S[t-1,i] - I[t,i]
+        R[t] <- R[t-1] + sum(I[t-1,])
     }
-    R[t] <- R[t-1] + sum(I[t-1,,])
-    Iobs[t] <- rbinom(1,prob=reporting,size=sum(I[t,,]))
+    for (i in 1:spacenum){
+      temp <- spacenum
+        for (j in 1:spacenum){
+          temp <- temp - (1-beta[i,j])^I[t,j]
+        }
+    pSI[t,i] <- min(1, temp)
+    Iobs[t] <- rbinom(1,prob=reporting,size=sum(I[t,]))
   }
-  
+    
+    Iobs[t] <- rbinom(1,prob=reporting,size=sum(I[t,]))
+  }
   return(list(time=tvec, S=S, I=I, R=R, Iobs=Iobs))
   
 }
 
-aa <- simspaceCB(spacenum = 2, numobs=10, betatemp=0.03,N=10000)
+aa <- simspaceCB(spacenum = 2, numobs=20, betaw=0.005, betab=0.0001, N=2000,seed=4)
