13572299f0ce96c207dfd8e241fa028f72f8ce41
mli2830 <wzmichael.li@gmail.com>
2016-05-08 17:09:31 -0400

    I am still not doing NB right, movnig on to stan

--- simulator.CNB.R
+++ simulator.CNB.R
@@ -5,16 +5,17 @@ library(ggplot2)
 ##' @param numobs ending time
 ##' @param seed random number seed
 ##' @param reporting observation probability (1 by default)
-
+##' 
+  
 simm <- function(R0=2,i0=1,t0=1, numobs=10, N=1000, effprop=0.9,
-                 repMean=0.5, seed=NULL, disshape=0.1){
+                 repMean=0.5, seed=NULL, disshape=0.1, k=2){
   
   ## *all* infecteds recover in the next time step
   
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,numobs)
   n <- length(tvec)
-  I <- Iobs <- IMean <- pSI <- S <- R <- numeric(n)
+  I <- Iobs <- IMean <- obsMean <- pSI <- S <- R <- numeric(n)
   
   ##Initial conditions
   
@@ -24,20 +25,24 @@ simm <- function(R0=2,i0=1,t0=1, numobs=10, N=1000, effprop=0.9,
   R[1] <- N - N0
   beta <- exp(-R0/N0)
   pSI[1] <- 1 - (beta)^I[1]
+  IMean[1] <- 0
   
-  disobs <- rgamma(1,disshape,1)
-  Iobs[1] <- rnbinom(1,size=disobs,prob=repMean)
+  obsMean[1] <- rgamma(1,shape=k,scale=repMean*I[1]/k)
+  Iobs[1] <- rpois(1,obsMean[1])
   
   ## Generate the Unobserved process I, and observables:
   
   for (t in 2:n){
-    I[t] <- rnbinom(1,size=S[t-1],prob=pSI[t-1])
+    IMean[t] <- rgamma(1,shape=k,scale=pSI[t-1]*S[t-1]/k)
+    I[t] <- rpois(1,IMean[t])
     S[t] <- S[t-1] - I[t]
     R[t] <- R[t-1] + I[t-1]
     pSI[t] <- 1 - (beta)^I[t]
-    Iobs[t] <- rnbinom(1,size=I[t],prob=repMean)
+    obsMean[t] <- rgamma(1,shape=k,scale=repMean*I[t]/k)
+    Iobs[t] <- rpois(1,obsMean[t])
+    
   }
   
-  data.frame(time=tvec, S, I, R, Iobs,pSI)
+  data.frame(time=tvec, S, I, R,IMean,Iobs,obsMean,pSI)
   
 }
